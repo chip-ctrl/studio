1bcdd44840fb38d9629ebf5fca24ec496a286c1a
Chip Joyner <chip@joynerventures.com>
2025-11-11 22:01:09 +0000

    Add to brand logos in the "About" section

--- src/components/homepage/AboutPreview.tsx
+++ src/components/homepage/AboutPreview.tsx
@@ -54,6 +54,7 @@ const brands = [
   { name: 'Barcelona Wine Bar', logoUrl: 'https://images.squarespace-cdn.com/content/v1/5e38c9d8063d8f5d60544ca7/1581442385023-F2ZKV4CANWC25A5A4V59/BWB_logo-01.png?format=1500w' },
   { name: 'Levy', logoUrl: 'https://cdn.worldvectorlogo.com/logos/levy-restaurants.svg' },
   { name: 'Sbarro', logoUrl: 'https://cdn.worldvectorlogo.com/logos/sbarro-1.svg' },
+  { name: 'Au Bon Pain', logoUrl: 'https://cdn.worldvectorlogo.com/logos/au-bon-pain.svg' },
 ];
 
 export function AboutPreview() {
